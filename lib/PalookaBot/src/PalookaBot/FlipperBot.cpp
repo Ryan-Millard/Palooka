039f6ed8029155321249ed3f7f5541dc9a4605e5
Ryan-Millard <millardryandevon@gmail.com>
2025-03-12 22:58:10 +0200

    Add flipper (ESP32Servo) to FlipperBot class

--- lib/PalookaBot/src/PalookaBot/FlipperBot.cpp
+++ lib/PalookaBot/src/PalookaBot/FlipperBot.cpp
@@ -7,10 +7,12 @@ namespace PalookaBot
 
 	// ========== Public ==========
 	// The constructor sets up the wheels and powers up the robot by enabling the necessary voltage rails.
-	FlipperBot::FlipperBot(const byte LEFT_PWM_PIN, const byte LEFT_DIRECTION_PIN,
+	FlipperBot::FlipperBot(const byte FLIPPER_PIN,
+			const byte LEFT_PWM_PIN, const byte LEFT_DIRECTION_PIN,
 			const byte RIGHT_PWM_PIN, const byte RIGHT_DIRECTION_PIN,
 			const byte EN8V_PIN, const byte EN5V_PIN, const byte DVR_SLEEP_PIN)
-		: wheelRight(RIGHT_PWM_PIN, RIGHT_DIRECTION_PIN), // Initialize the right wheel with its designated pins.
+		: FLIPPER_MAX_ANGLE(180), FLIPPER_MIN_ANGLE(0),
+		wheelRight(RIGHT_PWM_PIN, RIGHT_DIRECTION_PIN), // Initialize the right wheel with its designated pins.
 		wheelLeft(LEFT_PWM_PIN, LEFT_DIRECTION_PIN, true /* Inverted */) // Invert the left wheel so that both wheels drive in the same forward direction.
 	{
 		// ========== Power up robot ==========
@@ -18,10 +20,30 @@ namespace PalookaBot
 		pinMode(EN8V_PIN, OUTPUT); // Set the pin controlling the 8V rail as an output.
 		pinMode(EN5V_PIN, OUTPUT); // Set the pin controlling the 5V rail as an output.
 		pinMode(DVR_SLEEP_PIN, OUTPUT); // Set the DVR sleep pin as an output.
+
 		// Activate the power rails and wake the motor driver.
 		digitalWrite(EN8V_PIN, HIGH); // Enable power on the 8V rail.
 		digitalWrite(EN5V_PIN, HIGH); // Enable power on the 5V rail.
 		digitalWrite(DVR_SLEEP_PIN, HIGH); // Wake the motor driver from sleep.
+
+		// ========== Initialize flipper ==========
+		flipper.setPeriodHertz(50);
+		flipper.attach(FLIPPER_PIN, 500, 2500);
+		flipper.write(90);
+	}
+
+	void FlipperBot::moveFlipper(byte angle)
+	{
+		// Ensure the angle is within the angle limits
+		angle = constrain(angle, FLIPPER_MIN_ANGLE, FLIPPER_MAX_ANGLE);
+		flipper.write(angle); // Set the flipper to the angle
+	}
+
+	void FlipperBot::flip()
+	{
+		moveFlipper(FLIPPER_MIN_ANGLE); // Put flipper against the ground
+		moveFlipper(FLIPPER_MAX_ANGLE); // Quickly lift flipper
+		moveFlipper(FLIPPER_MIN_ANGLE); // Put flipper against the ground
 	}
 
 	// The move() function takes x (turn) and y (forward/backward) inputs, each expected to be in the range [-1, 1].
--- lib/PalookaBot/src/PalookaBot/FlipperBot.h
+++ lib/PalookaBot/src/PalookaBot/FlipperBot.h
@@ -2,6 +2,7 @@
 #define PALOOKABOT_FLIPPERBOT_H
 
 #include <Arduino.h>
+#include <ESP32Servo.h>
 #include "Motor.h"
 
 namespace PalookaBot
@@ -11,6 +12,11 @@ namespace PalookaBot
 	class FlipperBot
 	{
 		private:
+			// ========== Flipper/Arm ==========
+			Servo flipper; // The arm on the robot that flips other robots
+			const byte FLIPPER_MAX_ANGLE;
+			const byte FLIPPER_MIN_ANGLE;
+
 			// ========== Wheels ==========
 			// Each Motor instance controls one wheel.
 			const Motor wheelLeft; // Motor B (configured as inverted to match the physical layout of the robot)
@@ -19,11 +25,16 @@ namespace PalookaBot
 		public:
 			// ========== Constructor function ==========
 			// The default parameters correspond to the recommended hardware configuration.
-			FlipperBot(const byte LEFT_PWM_PIN = 25, const byte LEFT_DIRECTION_PIN = 26,
+			FlipperBot(const byte FLIPPER_PIN = 14,
+					const byte LEFT_PWM_PIN = 25, const byte LEFT_DIRECTION_PIN = 26,
 					const byte RIGHT_PWM_PIN = 32, const byte RIGHT_DIRECTION_PIN = 33,
 					const byte EN8V_PIN = 16, const byte EN5V_PIN = 17, const byte DVR_SLEEP_PIN = 12);
 
-			// ========== Movement functions ==========
+			// ========== Flipper Movement functions ==========
+			void moveFlipper(byte angle);
+			void flip();
+
+			// ========== Wheel movement functions ==========
 			// move() allows for driving the robot using x (lateral/turning) and y (forward/backward) values.
 			// Inputs are expected to be within [-1, 1] and will be normalized if necessary.
 			void move(const float x, const float y) const;
@@ -31,7 +42,7 @@ namespace PalookaBot
 			// Note: Due to the inversion, moveLeftWheel causes a turn to the right and vice versa.
 			void moveLeftWheel(const short velocity) const; // Controls the left wheel's rotation.
 			void moveRightWheel(const short velocity) const; // Controls the right wheel's rotation.
-			// stopMoving() halts all movement by stopping both wheels.
+															 // stopMoving() halts all movement by stopping both wheels.
 			void stopMoving() const;
 	};
 }
